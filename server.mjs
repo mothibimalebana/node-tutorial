1862bbec67f8705260c4bb96e967c0135df32b4e
mothibimalebana <mothibi.mm12@gmail.com>
2025-04-04 17:51:38 +0200

    the app is fully functional as intended

--- server.mjs
+++ server.mjs
@@ -1,15 +1,21 @@
 import {createServer} from 'http';
 import url from 'url';
+import fs from 'fs'
 
-const server = createServer((req, res) => {
-    res.writeHead(200, {'content-type': 'text/html'});
-    
+const server = createServer((req, res) => {    
     const q = url.parse(req.url, true);
-    const currentPage = q.pathname
+    const currentPage = q.pathname;
+    const filename = '.' + currentPage + '.html';
+    fs.readFile(filename, (err, data) => {
+        if(err){
+            res.writeHead(404, {'content-type': 'text/html'})
+            return res.end('404 Not Found')
+        }
+        res.writeHead(200, {'content-type': 'text/html'});
+        res.write(data);
+        return res.end()
+    })
 
-    res.write(`${q.pathname}`);
-
-    res.end();
 }).listen(8080, () => {
     console.log('Server is listening')
 })
